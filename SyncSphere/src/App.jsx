6f2fdced280db3975c0dba219820669dc3cda78c
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-07-29 23:02:46 +0530

    structure changed

--- SyncSphere/src/App.jsx
+++ SyncSphere/src/App.jsx
@@ -4,12 +4,12 @@ import { Home } from "./Pages/Home";
 import { About } from "./Pages/About";
 import { Connect } from "./Pages/Connect";
 import { Contact } from "./Pages/Contact";
-import { HomeRough } from "./Pages/HomeRough";
 import { Registration } from "./Pages/Registration";
 import { AppLayout } from "./Components/Layout/AppLayout";
 import { Login } from "./Pages/Login";
 import { UserProfile } from "./Pages/UserProfile";
 import User from "./Pages/User";
+import { ProfileEdit } from "./Pages/ProfileEdit";
 
 export const App = () => {
   const router = createBrowserRouter([
@@ -35,13 +35,9 @@ export const App = () => {
           element: <Contact />,
         },
         {
-          path: "/HomeRough",
-          element: <HomeRough />,
-        },
-        {
           path: "/user",
           element: <User />,
-       },
+        },
       ],
     },
     {
